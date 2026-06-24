3472b9d7e9a2dd416b63c3bbe2dd11b73f1185df
lokicsit <loknathadhikariadhikari@gmail.com>
2026-06-24 18:47:34 +0545

    Day 1

---
+++ modules.py
@@ -0,0 +1,19 @@
+import pyjokes
+# this print random joke
+
+
+joke = pyjokes.get_joke()
+print(joke) 
+
+
+
+
+# single line comment
+# thank you
+#  for watching
+#  this programming
+"""
+multile line comment
+thank for the see this programming .
+
+"""
---
+++ problem2.py
@@ -0,0 +1,4 @@
+import pyttsx3  
+engine = pyttsx3.init()
+engine.say("what is your name and where are you from ?")
+engine.runAndWait()
