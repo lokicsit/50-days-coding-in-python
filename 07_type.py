d622fd7939b7bec2dc1e665db50534ca8c9b131d
lokicsit <loknathadhikariadhikari@gmail.com>
2026-07-01 20:55:33 +0545

    Day 7

---
+++ 07_type.py
@@ -0,0 +1,32 @@
+a= 31
+t = type(a)
+print(t)
+
+
+
+#float      
+a= 31.1243
+t = type(a)
+print(t)
+
+
+#string changeprocess
+a= "322.23" 
+b= float(a)
+T= type(a)
+print(T)
+
+
+#tpye change code
+a= "322.23" 
+b= float(a)
+T= type(a)
+print(b)
+
+
+
+#float to int
+a= 31.1243
+b= int(a)
+t = type(b)
+print(t)
